b6e4b7e31147e9ea7042a6524fdca028e5ea11cb
Normsmiway <normsmiway@gmail.com>
2021-09-07 16:40:44 +0100

    update

--- UseCases/CreateWeather/CreateWeatherCommand.cs
+++ UseCases/CreateWeather/CreateWeatherCommand.cs
@@ -1,7 +1,5 @@
 ﻿using MediatR;
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace MediatorDemo.UseCases.CreateWeather
 {
@@ -25,21 +23,6 @@ namespace MediatorDemo.UseCases.CreateWeather
             Date = DateTime.UtcNow;
             TemperatureC = tempC;
             Summary = Summaries[tempC];
-        }
-    }
-
-
-    internal sealed class CreateWeatherCommandHandler : IRequestHandler<CreateWeatherCommand, WeatherForecast>
-    {
-
-        public async Task<WeatherForecast> Handle(CreateWeatherCommand request, CancellationToken cancellationToken)
-        {
-            return new WeatherForecast()
-            {
-                Date = request.Date,
-                Summary = request.Summary,
-                TemperatureC = request.TemperatureC
-            };
         }
     }
 }
---
+++ UseCases/CreateWeather/CreateWeatherCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatorDemo.UseCases.CreateWeather
+{
+    internal sealed class CreateWeatherCommandHandler : IRequestHandler<CreateWeatherCommand, WeatherForecast>
+    {
+
+        public async Task<WeatherForecast> Handle(CreateWeatherCommand request, CancellationToken cancellationToken)
+        {
+            return new WeatherForecast()
+            {
+                Date = request.Date,
+                Summary = request.Summary,
+                TemperatureC = request.TemperatureC
+            };
+        }
+    }
+}
+
